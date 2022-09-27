9de3ea004f4c7553ec9b3425ee7a1e3e782a01b2
Mads <mbm92@hotmail.dk>
2022-09-27 20:43:54 +0200

    ForestTR01 - room added
    Added room for Forest

---
+++ Spike-TaleOfAMissingDog/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+	public float moveSpeed = 5f;
+
+	public Rigidbody2D rb;
+	public Animator animator; 
+
+	private Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // handle input 
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        movement = movement.normalized;
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+
+        animator.SetFloat("Speed", movement.sqrMagnitude); 
+
+    }
+
+
+    void FixedUpdate()  // calls 50/sec
+    {
+	    //handle movement 
+        rb.MovePosition(rb.position + movement * moveSpeed *Time.fixedDeltaTime);
+    }
+}
