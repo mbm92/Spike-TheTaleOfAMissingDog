717b20346de2cc8a244f82f792d398869452e87d
Julia <jjulkaxd@gmail.com>
2022-11-07 22:07:52 +0100

    Healthbar

---
+++ Spike-TaleOfAMissingDog/Assets/Scripts/Enemyhealth.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Enemyhealth : MonoBehaviour
+{
+    public float maxHealth;
+    public float health;
+
+    public Slider slider;
+
+    void Start()
+    {
+        health = maxHealth;
+        slider.value = CalculateHealth();
+    }
+
+    void Update()
+    {
+        slider.value = CalculateHealth();
+
+        if (health <= 0 )
+        {
+            //gameObject.Move(Vector3.zero,false,false);
+            //WaitForSeconds(3);
+            Destroy(gameObject);
+        }
+
+    }
+    float CalculateHealth()
+    {
+        return health/ maxHealth;
+    }
+}
