80507a11c461b3a27cdc526fa69ff55a6c9536a8
Julia <julia@natulique.com>
2022-11-08 10:30:22 +0100

    PlayerHealth

---
+++ Spike-TaleOfAMissingDog/Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    //public float enemyCooldown = 2;
+    //public float damage = 6;
+
+    //private bool playerInRange = false;
+    //private bool canAttack = true;
+
+    //private void Update()
+    //{
+    //    if (playerInRange && canAttack)
+    //    {
+    //        GameObject.Find("Player").GetComponent<ControllerForPlayer>().currentHealth -= damage;
+    //        StartCoroutine(AttackCooldown());
+    //    }
+    //}
+
+    //void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.gameObject.CompareTag("Player")) ;
+    //    {
+    //        playerInRange = true;
+    //    }
+    //}
+    //void OnTriggerExit(Collider other)
+    //{
+    //    if (other.gameObject.CompareTag("Player")) ;
+    //    {
+    //        playerInRange = false;
+    //    }
+    //}
+    //IEnumerator AttackCooldown()
+    //{
+    //    canAttack = false;
+    //    yield return new WaitForSeconds(enemyCooldown);
+    //    canAttack = true;
+    //}
+}
