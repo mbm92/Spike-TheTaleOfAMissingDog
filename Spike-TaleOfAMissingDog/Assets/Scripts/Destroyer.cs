67f1c04e5926d58c4610f07d45031d62a92b58ea
Mads <mbm92@hotmail.dk>
2022-11-15 20:23:38 +0100

    fixed grass texture problem for Forest Night level

--- Spike-TaleOfAMissingDog/Assets/Scripts/Destroyer.cs
+++ Spike-TaleOfAMissingDog/Assets/Scripts/Destroyer.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Debug = UnityEngine.Debug;
 
 public class Destroyer : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D other)
     {
+        Debug.Log("StartRoom Collide with: " + other.tag + ", " + other.name);
         Destroy(other.gameObject);
         // destroy this.spawnPoint GameoBject, so it will not destroy other objects.
         Destroy(gameObject);
